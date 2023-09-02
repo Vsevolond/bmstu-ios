b7fa740ebacaf7f8fa1e2f4928526d67185ecf59
Vsevolod Donchenko <vsevolond@gmail.com>
2023-09-02 20:07:05 +0300

    PROD: started project

---
+++ BmstuSchedule/BmstuSchedule/BmstuScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BmstuScheduleApp.swift
+//  BmstuSchedule
+//
+//  Created by Всеволод on 02.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct BmstuScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
