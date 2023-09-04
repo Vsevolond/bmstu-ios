ce4622f2a2f751241931b0dbf5218946495c9606
Vsevolond <vsevolond@gmail.com>
2023-09-04 23:41:38 +0300

    TRUNK: added tab bar

--- BmstuSchedule/BmstuSchedule/ContentView.swift
+++ BmstuSchedule/BmstuSchedule/ContentView.swift
@@ -2,31 +2,91 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject var model = ScheduleViewModel()
-    @State var isFullName = false
+    @State private var selectedIndex: Int = 0
     
     var body: some View {
-        TabView {
-            ForEach(model.days.items) { day in
-                List {
-                    ForEach(day.lessons.items) { lesson in
-                        LessonView(lesson: lesson)
+        VStack {
+            TabView(selection: $selectedIndex) {
+                ForEach(model.days.items) { day in
+                    List {
+                        ForEach(day.lessons.items) { lesson in
+                            LessonView(lesson: lesson)
+                        }
+                        .listRowSeparator(.hidden)
+                        .listRowBackground(
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(Color.white)
+                                .padding(5)
+                        )
                     }
-                    .listRowSeparator(.hidden)
-                    .listRowBackground(
-                        RoundedRectangle(cornerRadius: 20)
-                            .fill(Color.white)
-                            .padding(5)
-                    )
                 }
-//                .tabItem {
-//                    Text("\(day.day)")
-//                }
+            }
+            .tabViewStyle(.page(indexDisplayMode: .never))
+            .onAppear {
+                model.fetchData()
+            }
+
+            TabBarView(tabbarItems: ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"], selectedIndex: $selectedIndex)
+                .padding(.horizontal)
+        }
+    }
+}
+
+struct TabBarView: View {
+    var tabbarItems: [String]
+
+    @Binding var selectedIndex: Int
+    @Namespace private var menuItemTransition
+
+    var body: some View {
+        ScrollViewReader { scrollView in
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(tabbarItems.indices, id: \.self) { index in
+                     
+                        TabbarItem(name: tabbarItems[index], isActive: selectedIndex == index, namespace: menuItemTransition)
+                            .onTapGesture {
+                                withAnimation(.easeInOut) {
+                                    selectedIndex = index
+                                }
+                            }
+                    }
+                }
+            }
+            .padding()
+            .background(Color(.systemGray6))
+            .cornerRadius(25)
+            .onChange(of: selectedIndex) { index in
+                withAnimation {
+                    scrollView.scrollTo(index, anchor: .center)
+                }
             }
         }
-        .tabViewStyle(.page(indexDisplayMode: .always))
-        .indexViewStyle(.page(backgroundDisplayMode: .always))
-        .onAppear {
-            model.fetchData()
+    }
+}
+
+struct TabbarItem: View {
+    var name: String
+    var isActive: Bool = false
+    let namespace: Namespace.ID
+ 
+    var body: some View {
+        if isActive {
+            Text(name)
+                .bold()
+                .font(.subheadline)
+                .padding(.horizontal)
+                .padding(.vertical, 4)
+                .foregroundColor(.white)
+                .background(Capsule().foregroundColor(.blue))
+                .matchedGeometryEffect(id: "highlightmenuitem", in: namespace)
+        } else {
+            Text(name)
+                .bold()
+                .font(.subheadline)
+                .padding(.horizontal)
+                .padding(.vertical, 4)
+                .foregroundColor(.black)
         }
     }
 }
